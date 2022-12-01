ba38d945912660579079ae39648227425e5f552f
kra-y <keitharaneo@gmail.com>
2022-12-01 16:43:06 -0700

    made change to misc file

--- misc.R
+++ misc.R
@@ -1,6 +1,19 @@
 #miscellaneous scritp not for my github repo
-data1<-read.csv("/Users/kraymac/Documents/R Projects/WSU Dashboards/5-year Compiled Data - Hired within last year.csv")
-
+FilePath<-"/Users/kraymac/Documents/R Projects/WSU Dashboards/"
+FileName<-"5-year Compiled Data - Hired within last year.csv"
+read.csv(FilePath+FileName)
+data1<-read.csv(paste0(FilePath,FileName))
+#data1<-read.csv("/Users/kraymac/Documents/R Projects/WSU Dashboards/5-year Compiled Data - Hired within last year.csv")
+getwd()
 summary(data1)
 head(data1$Sector)
 head(data1)
+class(data1[,1])
+
+###############
+#can we make a function that takes a data set as an argument and automatically finds the factor or character columns and replaces the "" values
+# with a string that states "No {column_name} Information"
+
+Character_No_Info<-function(x){
+  
+}
